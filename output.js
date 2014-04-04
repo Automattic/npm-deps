ce14d0fcaf67e3e09d244375476cde2ac558ece2
Marco Aurelio <thecoreh@gmail.com>
2014-04-03 22:11:47 -0300

    Allow outputting to a file instead of stdout.

--- output.js
+++ output.js
@@ -1,26 +1,30 @@
 var fs = require('fs');
 var path = require('path');
 
-function output(deps) {
-  var pkgJson;
-  try {
-    pkgJson = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
-  } catch (e) {
-    pkgJson = {};
-  }
-  pkgJson.dependencies = {};
-  pkgJson.devDependencies = {};
+module.exports = function(app) {
+  return function output(deps) {
+    var pkgJson;
+    try {
+      pkgJson = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+    } catch (e) {
+      pkgJson = {};
+    }
+    pkgJson.dependencies = {};
+    pkgJson.devDependencies = {};
   
-  for (var name in deps) {
-    var dep = deps[name];
-    if (dep.type == 'development') {
-      pkgJson.devDependencies[name] = dep.version;
+    for (var name in deps) {
+      var dep = deps[name];
+      if (dep.type == 'development') {
+        pkgJson.devDependencies[name] = dep.version;
+      } else {
+        pkgJson.dependencies[name] = dep.version;
+      }
+    }
+    
+    if (app.output) {
+      fs.writeFileSync(app.output, JSON.stringify(pkgJson, null, 2), 'utf-8');
     } else {
-      pkgJson.dependencies[name] = dep.version;
+      console.log(JSON.stringify(pkgJson, null, 2));
     }
   }
-  
-  console.log(JSON.stringify(pkgJson, null, 2));
 }
-
-module.exports = output;
